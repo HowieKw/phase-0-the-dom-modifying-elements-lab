de8d194ab242410d03bf2d0385dfc48ac8b93a4b
Howard Kwon <howie.ykwon70@gmail.com>
2021-07-09 18:25:45 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.innerHTML = "Howard is the champion";
